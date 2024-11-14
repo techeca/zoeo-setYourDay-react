3f5ec7910bb0afc37cc6ac1cc68e0fe1d0a6df82
techeca <jp.vasquez@outlook.com>
2024-11-14 20:19:32 -0300

    Request para documentos y usuarios.

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -1,9 +1,13 @@
+const API_URL = 'http://localhost:3000'
+
 export const handleGenerateDocument = async (documento) => {
+    //Solicitud para descargar documento
     try {
-        const response = await fetch('http://localhost:3000/api/process-document', {
+        console.log(documento);
+        const response = await fetch(`${API_URL}/api/process-document`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({documento:documento})
+            body: JSON.stringify({ documento: documento._id })
         });
 
         if (response.ok) {
@@ -11,7 +15,7 @@ export const handleGenerateDocument = async (documento) => {
             const url = window.URL.createObjectURL(blob);
             const a = document.createElement('a');
             a.href = url;
-            a.download = 'modified-template.docx';
+            a.download = `TEA-${documento._id}.docx`;
             document.body.appendChild(a);
             a.click();
             a.remove();
@@ -19,24 +23,156 @@ export const handleGenerateDocument = async (documento) => {
             console.error('Failed to download the document');
         }
     } catch (error) {
-        console.error('Error generating document:', error);
+        console.error('Error al generar el documento:', error);
     }
 };
 
-export const handleUpdateDocument = async (key, documento, texto) => {
+export const handleUpdateDocument = async (key, documento, texto, docId) => {
     try {
-        const response = await fetch('http://localhost:3000/api/update-document', {
+        const response = await fetch(`${API_URL}/api/update-document`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ key: key, documento: documento, texto: texto })
+            body: JSON.stringify({ key: key, documento: documento, texto: texto, docId: docId })
         });
 
         if (response.ok) {
-            console.log('Document updated');
+            //console.log('Document updated');
+            const data = await response.json();
+            return { message: data.message }
         } else {
-            console.error('Failed to download the document');
+            console.log(response);
+            console.error('Error al actualizar el documento desde la API');
         }
     } catch (error) {
-        console.error('Error generating document:', error);
+        console.error('Error al enviar la actualización del documento:', error);
     }
 };
+
+export const handleCreateDocument = async (document) => {
+    try {
+        const response = await fetch(`${API_URL}/api/create-document`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ document: document })
+        })
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const jsonData = await response.json();
+        return jsonData;
+    } catch (error) {
+        console.error('Error al crear el documento:', error);
+    }
+}
+
+export const getAllDocuments = async () => {
+    try {
+        const response = await fetch(`${API_URL}/api/list-documents`, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' }
+        })
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const jsonData = await response.json();
+        return jsonData;
+    } catch (error) {
+        console.error('Error al obtener los documentos:', error);
+    }
+}
+
+export const getDocument = async (documentId) => {
+    try {
+        const response = await fetch(`${API_URL}/api/document/${documentId}`, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json'}
+        });
+        const jsonData = await response.json();
+        return { document: jsonData, message: 'Documento encontrado' };
+    } catch (error) {
+        console.error('Error al buscar el documento:', error);
+        return { message: 'Error al buscar el documento' }
+    }
+}
+
+export const deleteDocument = async (documentId) => {
+    try {
+        const response = await fetch(`${API_URL}/api/delete-document`, {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({ id: documentId })
+        }); 
+
+        const jsonData = await response.json();
+        return { document: jsonData, message: 'Documento eliminado' };
+    } catch (error) {
+        return { message: 'Error al eliminar el documento' }
+    }
+}
+
+export const handleCreateUser = async (email, password, username) => {
+    try {
+        const response = await fetch(`${API_URL}/api/auth/register`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email, password, username })
+        });
+        if (response.ok) {
+            const data = await response.json();
+            return { message: data.message };
+        } else {
+            const errorData = await response.json();
+            return { message: errorData.error || 'Error al crear el usuario' };
+        }
+    } catch (error) {
+        console.error('Error al crear el usuario:', error);
+        return { message: 'Error al conectar con el servidor' };
+    }
+};
+
+export const getAllProfesional = async () => {
+    try {
+        const response = await fetch(`${API_URL}/api/auth/allProfesional`, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' }
+        })
+        if (response.ok) {
+            const data = await response.json();
+            return { message: data.message, pros: data.pros };
+        } else {
+            const errorData = await response.json();
+            return { message: errorData.error || 'Error al obtener los profesionales' };
+        }
+    } catch (error) {
+        console.error('Error al obtener los profesionales:', error);
+        return { message: 'Error al conectar con el servidor' };
+    }
+}
+
+export const handleUserLogin = async (email, password) => {
+    try {
+        const response = await fetch(`${API_URL}/api/auth/login`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email, password })
+        });
+        const data = await response.json();
+
+        if (response.status === 200) {
+            // Inicio de sesión exitoso
+            return { message: data.message, user: data.user, token: data.token };
+        } else if (response.status === 401) {
+            // Credenciales inválidas
+            return { message: data.message };
+        } else if (response.status === 500) {
+            // Error interno del servidor
+            return { message: 'Error en el servidor. Intente nuevamente más tarde.' };
+        } else {
+            // Otro tipo de error
+            return { message: data.message || 'Ocurrió un error inesperado.' };
+        }
+    } catch (error) {
+        console.error("Error en el inicio de sesión:", error);
+        return { message: 'Error de conexión al servidor' };
+    }
+}
