207c014d25d03093cb63be5372dad31d15ab2fae
techeca <jp.vasquez@outlook.com>
2024-11-14 20:54:55 -0300

    Nueas rutas, profile, team y admin/account.

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,12 @@
 
 import Panel from "./components/Panel"
 import Login from "./components/Login"
+import Profile from "./components/Profile"
+import Team from "./components/Team"
 import App from "./App"
+import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary"
+import Admin from "./components/Admin"
+import Accounts from "./components/Admin/Accounts"
 
 const router = [
     {
@@ -14,7 +19,26 @@ const router = [
             },
             {
                 path: 'login',
-                element: <Login />
+                element: <Login />,
+                errorElement: <ErrorBoundary />
+            },
+            {
+                path: 'profile',
+                element: <Profile />
+            },
+            {
+                path: 'team',
+                element: <Team />
+            },
+            {
+                path: 'Admin',
+                element: <Admin />,
+                children: [
+                    {
+                        path: 'accounts',
+                        element: <Accounts />
+                    }
+                ]
             }
         ]
     }
