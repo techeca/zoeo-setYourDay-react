37cb7109784633ee0c410a7d189f0e09cb3f12a5
techeca <jp.vasquez@outlook.com>
2024-11-14 23:51:02 -0300

    Conexion a bd y router de API.

---
+++ api/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import documentRouter from './routes/DocumentRoute.js';
+import cors from 'cors';
+import routerAuth from './routes/AuthRoute.js'
+//import bodyParser from 'body-parser';
+
+const app = express();
+//const port = process.env.API_PORT;
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+function setupMiddleware(){
+    app.use(express.json());
+    app.use(cors())
+}
+
+function setupRoutes(){
+    app.use('/api', documentRouter);
+    app.use('/api/auth', routerAuth)
+}
+
+export function startServer(PORT){
+    setupMiddleware()
+    setupRoutes()
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}
