10a332b1679816873120519a89749d8ba29a826c
techeca <jp.vasquez@outlook.com>
2024-11-14 23:52:08 -0300

    Template de TEA y configuración del proyecto.

--- api/index.js
+++ api/index.js
@@ -1,15 +1,16 @@
-import express from 'express';
-//import bodyParser from 'body-parser';
-import router from './routes/documentRoute.js';
-import cors from 'cors'
+import { startServer } from "./server.js";
+import { connectDB } from "./database.js";
 
-const app = express();
-const port = 3000;
+const PORT = process.env.API_PORT || 3000;
 
-app.use(express.json());
-app.use(cors())
-app.use('/api', router);
+async function init() {
+  try {
+    await connectDB()
+    startServer(PORT)
+  } catch (error) {
+    console.error("Error initializing the application:", error);
+    process.exit(1);
+  }
+}
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+init()
