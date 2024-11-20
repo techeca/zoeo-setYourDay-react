9ff716bd8bb2a1bbaf2916a2ab63b2e0babb7ea5
techeca <jp.vasquez@outlook.com>
2024-11-20 02:48:40 -0300

    Cambio de nombre de variable para render.

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import { startServer } from "./server.js";
 import { connectDB } from "./database.js";
 
-const PORT = process.env.API_PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 async function init() {
   try {
