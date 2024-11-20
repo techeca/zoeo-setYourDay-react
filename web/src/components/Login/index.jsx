3e8b3dd063f3446e243958aa6ea7f5b5115ba827
techeca <jp.vasquez@outlook.com>
2024-11-20 00:15:21 -0300

    Cambio de diseño de componente login.

--- web/src/components/Login/index.jsx
+++ web/src/components/Login/index.jsx
@@ -47,33 +47,34 @@ export default function Login() {
     };
 
     return (
-        <div className="mx-auto flex w-full max-w-xl flex-col gap-6 mt-24">
-            <div className="flex flex-col items-center">
-                <h1 className="text-3xl font-semibold">Ingreso</h1>
-                <p className="text-sm">Escribe tus datos para acceder a tu cuenta</p>
+        <div className="mx-auto flex w-full max-w-xl flex-col mt-24">
+            <div className="bg-gray-3 p-6 rounded-lg">
+                <div className="flex flex-col items-center">
+                    <h1 className="text-3xl font-semibold">Ingreso</h1>
+                    {/*<p className="text-sm">Escribe tus datos para acceder a tu cuenta</p>*/}
+                </div>
+                <form onSubmit={(e) => handleLogin(e)} className="form-group mt-6">
+
+                    <div className="form-field">
+                        <label className="form-label">Correo</label>
+
+                        <input id='correo' type="email" className="bg-gray-2 w-full rounded-xl p-2 hover:bg-gray-1 focus:bg-gray-1 outline-none pl-3 placeholder:text-gray-10" placeholder="correo@email.com" value={formValues.correo || ""} onChange={handleInputChange} />
+                    </div>
+
+                    <div className="form-field">
+                        <label className="form-label">Contraseña</label>
+                        <div className="form-control">
+                            <input placeholder="******" type="password" id='contrasena' value={formValues.contrasena || ""} onChange={handleInputChange} className="bg-gray-2 w-full rounded-xl p-2 hover:bg-gray-1 focus:bg-gray-1 outline-none pl-3 placeholder:text-gray-10" />
+                        </div>
+                    </div>
+
+                    <div className="form-field pt-5 pb-3">
+                        <div className="form-control justify-between">
+                            <button type="submit" className="btn btn-primary w-full">Enviar</button>
+                        </div>
+                    </div>
+                </form>
             </div>
-            <form onSubmit={(e) => handleLogin(e)} className="form-group">
-                <div className="form-field">
-                    <label className="form-label">Correo</label>
-
-                    <input placeholder="correo@email.com" type="email" id='correo' value={formValues.correo || ""} onChange={handleInputChange} className="input max-w-full" />
-                    <label className="form-label">
-                        <span className="form-label-alt">Ingresa un email válido.</span>
-                    </label>
-                </div>
-                <div className="form-field">
-                    <label className="form-label">Contraseña</label>
-                    <div className="form-control">
-                        <input placeholder="******" type="password" id='contrasena' value={formValues.contrasena || ""} onChange={handleInputChange} className="input max-w-full" />
-                    </div>
-                </div>
-                <div className="form-field pt-5">
-                    <div className="form-control justify-between">
-                        <button type="submit" className="btn btn-primary w-full">Enviar</button>
-                    </div>
-                </div>
-
-            </form>
         </div>
     )
 }
