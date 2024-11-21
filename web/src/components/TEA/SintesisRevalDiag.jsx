531632e3a95b435570e3372293972005e9922cb5
techeca <jp.vasquez@outlook.com>
2024-11-21 20:39:52 -0300

    Cambio de estilos de inputs.

--- web/src/components/TEA/SintesisRevalDiag.jsx
+++ web/src/components/TEA/SintesisRevalDiag.jsx
@@ -2,43 +2,7 @@ import { useState, useEffect } from "react";
 import { useDocument } from "../../contexts/DocumentContext";
 import { handleUpdateDocument } from "../../utils/requests";
 import { useAlert } from "../../contexts/AlertContext";
-import RichTextEditor from "../RichTextEditor";
-
-const initialValue = [
-    {
-        type: 'paragraph',
-        children: [
-            { text: 'This is editable ' },
-            { text: 'rich', bold: true },
-            { text: ' text, ' },
-            { text: 'much', italic: true },
-            { text: ' better than a ' },
-            { text: '<textarea>', code: true },
-            { text: '!' },
-        ],
-    },
-    {
-        type: 'paragraph',
-        children: [
-            {
-                text: "Since it's rich text, you can do things like turn a selection of text ",
-            },
-            { text: 'bold', bold: true },
-            {
-                text: ', or add a semantically rendered block quote in the middle of the page, like this:',
-            },
-        ],
-    },
-    {
-        type: 'block-quote',
-        children: [{ text: 'A wise quote.' }],
-    },
-    {
-        type: 'paragraph',
-        align: 'center',
-        children: [{ text: 'Try it out for yourself!' }],
-    },
-]
+//import RichTextEditor from "../RichTextEditor";
 
 export default function SintesisRevalDiag() {
     const { document, documentSelected } = useDocument();
