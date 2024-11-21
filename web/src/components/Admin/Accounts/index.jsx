7c218692be7e6f1752e45c1410fcfa83a8dd7709
techeca <jp.vasquez@outlook.com>
2024-11-21 20:20:30 -0300

    Se incluyen iconos para datos de tarjetas de cuentas.

--- web/src/components/Admin/Accounts/index.jsx
+++ web/src/components/Admin/Accounts/index.jsx
@@ -156,9 +156,15 @@ export default function Accounts() {
                                 </div>
                             </div>
                         </div>
-                        <div className="bg-gray-2 p-3 rounded-md mt-2 flex flex-col gap-2">
-                            <p className=""><span className="font-semibold"></span><span className="w-full badge text-gray-100/70">Nombre: {pro.username}</span></p>
-                            <p className=""><span className="font-semibold"></span><span className="w-full badge text-gray-100/70">Correo: {pro.email}</span></p>
+                        <div className="bg-gray-2 p-3 rounded-xl mt-2 flex flex-col gap-2">
+                            <p className=""><span className="font-semibold"></span><span className="w-full badge text-gray-100/70">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" className="size-4 mr-1"><g fill="none" stroke="currentColor"><path strokeLinejoin="round" d="M4 18a4 4 0 0 1 4-4h8a4 4 0 0 1 4 4a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2Z"></path><circle cx={12} cy={7} r={3}></circle></g></svg>
+                                Nombre: <span className="ml-1 font-normal">{pro.username}</span></span>
+                            </p>
+                            <p className=""><span className="font-semibold"></span><span className="w-full badge text-gray-100/70 flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" className="size-4 mr-1"><g fill="none"><path fill="currentColor" d="M3 5v-.5a.5.5 0 0 0-.5.5zm18 0h.5a.5.5 0 0 0-.5-.5zM3 5.5h18v-1H3zM20.5 5v12h1V5zM19 18.5H5v1h14zM3.5 17V5h-1v12zM5 18.5A1.5 1.5 0 0 1 3.5 17h-1A2.5 2.5 0 0 0 5 19.5zM20.5 17a1.5 1.5 0 0 1-1.5 1.5v1a2.5 2.5 0 0 0 2.5-2.5z"></path><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="m3 5l9 9l9-9"></path></g></svg>
+                                Correo: <span className="ml-1 font-normal">{pro.email}</span></span>
+                            </p>
                         </div>
                     </div>
                 ))}
