ad26e8ac6d1be96370cc4919b77042270a0453a2
techeca <jp.vasquez@outlook.com>
2024-11-29 19:38:34 -0300

    cambio de selector de documento.

--- web/src/components/DocumentSelector/index.jsx
+++ web/src/components/DocumentSelector/index.jsx
@@ -2,15 +2,22 @@ import { cross } from '../../utils/icons';
 import { useDocument } from '../../contexts/DocumentContext';
 import useDocumentAction from '../../hooks/useDocumentAction';
 import { useAlert } from '../../contexts/AlertContext';
+import { useState } from 'react';
 
 export default function DocumentSelector() {
-    const { documentSelected, setDocumentSelected, youDay, resetDocument } = useDocument();
+    const { documentSelected, setDocumentSelected, youDay, resetDocument, setYouDay } = useDocument();
+    const [ documentName, setDocumentName] = useState('');
     const { generateDocument } = useDocumentAction();
     const { showAlert } = useAlert();
 
     const handleGenerate = async () => {
-        const { message, id } = await generateDocument();
+        const { message, id } = await generateDocument(documentName);
         showAlert(message, 'success', id)
+    }
+
+    const handleClose = () => {
+        setDocumentName('');
+        setYouDay(false)
     }
 
     return (
@@ -18,7 +25,7 @@ export default function DocumentSelector() {
             {/* Header + Close Button */}
             <div className='border-b-[1px] border-gray-50/10 flex justify-between items-center pb-2'>
                 <label className='text-slate-12'>Seleccione un documento</label>
-                <button className='hover:opacity-80 border-0 text-sm rounded-full' onClick={resetDocument}>
+                <button className='hover:opacity-80 border-0 text-sm rounded-full' onClick={handleClose}>
                     {cross}
                 </button>
             </div>
@@ -26,8 +33,8 @@ export default function DocumentSelector() {
             {/* Document Dropdown */}
             <div className='flex gap-3 mt-1'>
                 <select
-                    value={documentSelected}
-                    onChange={(e) => setDocumentSelected(e.target.value)}
+                    value={documentName}
+                    onChange={(e) => setDocumentName(e.target.value)}
                     className='w-full placeholder:text-white/30 bg-gray-2 border-0 text-sm text-white/50 py-2 px-2 rounded-md'
                 >
                     <option value="" hidden disabled>Seleccionar</option>
@@ -39,7 +46,7 @@ export default function DocumentSelector() {
             <div className='flex my-auto mx-auto'>
                 <button 
                     className="btn bg-gray-2 hover:opacity-80 rounded-md" 
-                    disabled={!documentSelected} 
+                    disabled={!documentName} 
                     onClick={() => handleGenerate()}
                 >
                     Continuar
